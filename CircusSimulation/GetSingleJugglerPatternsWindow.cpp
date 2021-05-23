96c28d3496463982ac9ac06943b1eb45d0e79bf2
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-05-23 19:24:26 +0100

    20210523 resize spin controls

--- CircusSimulation/GetSingleJugglerPatternsWindow.cpp
+++ CircusSimulation/GetSingleJugglerPatternsWindow.cpp
@@ -7,7 +7,10 @@ GetSingleJugglerPatternsWindow::GetSingleJugglerPatternsWindow(wxWindow* parent)
 	: wxWindow(parent, wxID_ANY)
 {
 	wxFlexGridSizer* sz_0 = new wxFlexGridSizer(3, 2, 0, 0);
-	
+	sz_0->SetFlexibleDirection(wxBOTH);
+	sz_0->AddGrowableCol(0);
+	sz_0->AddGrowableCol(1);
+
 	wxStaticText* st_numberBalls = new wxStaticText(this, wxID_ANY, "Balls in pattern :");
 	wxStaticText* st_numberThrows = new wxStaticText(this, wxID_ANY, "Throws in pattern :");
 	wxStaticText* st_maxThrowHeight = new wxStaticText(this, wxID_ANY, "Max throw height :");
@@ -24,15 +27,17 @@ GetSingleJugglerPatternsWindow::GetSingleJugglerPatternsWindow(wxWindow* parent)
 		wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS,
 		0, 100);
 
+	wxSizerFlags st_flags = wxSizerFlags().Center();
+	wxSizerFlags sp_flags = wxSizerFlags().Expand();
 
-	sz_0->Add(st_numberBalls);
-	sz_0->Add(sp_numberBalls);
+	sz_0->Add(st_numberBalls, st_flags);
+	sz_0->Add(sp_numberBalls, sp_flags);
 
-	sz_0->Add(st_numberThrows);
-	sz_0->Add(sp_numberThrows);
+	sz_0->Add(st_numberThrows, st_flags);
+	sz_0->Add(sp_numberThrows, sp_flags);
 	
-	sz_0->Add(st_maxThrowHeight);
-	sz_0->Add(sp_maxThrowHeight);
+	sz_0->Add(st_maxThrowHeight, st_flags);
+	sz_0->Add(sp_maxThrowHeight, sp_flags);
 
 	SetSizer(sz_0);
 }
