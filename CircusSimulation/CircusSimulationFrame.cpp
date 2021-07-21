e4a522d4d22414270293a98af6f6624ba01cca32
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-07-21 13:49:30 +0100

    Merge

--- CircusSimulation/CircusSimulationFrame.cpp
+++ CircusSimulation/CircusSimulationFrame.cpp
@@ -1,8 +1,7 @@
 #include <wx\spinctrl.h>
 
 #include "Settings.h"
-#include "BallStatusWindow.h"
-#include "SingleJugglerWindow.h"
+#include "SingleJugglerWithRandomColourPicker.h"
 #include "PoolJugglers2dWindow.h"
 
 #include "CircusSimulationFrame.h"
@@ -60,26 +59,18 @@ CircusSimulationFrame::CircusSimulationFrame(const wxString & title, const wxPoi
 
 	wxBoxSizer * sz_0 = new wxBoxSizer(wxHORIZONTAL);
 
-	// Set up values for ball drawing window sizing.
-
-	unsigned int window_ratios_x[3];
-	unsigned int window_ratios_y[4];
-
-	window_ratios_x[0] = 3u; window_ratios_x[1] = 5u; window_ratios_x[2] = 1u;
-	window_ratios_y[0] = 2u; window_ratios_y[1] = 2u; window_ratios_y[2] = 7u; window_ratios_y[3] = 1u;
-
-	window_1 = new SingleJugglerWindow(this, &(window_ratios_x[0]), &(window_ratios_y[0]), 0.4);
+	window_1 = new SingleJugglerWithRandomColourPicker(this);
 	window_2 = new PoolJugglers2dWindow(this, 3U);
-	// window_2 = new BallStatusWindow(this);
 	
 	GetSingleJugglerPatternsWindow* window_3 = new GetSingleJugglerPatternsWindow(this, WINDOW_SINGLE_JUGGLER_PATTERNS);
 
-	pattern_handler.AddWindow(window_1);
+	pattern_handler.AddWindow(window_1->GetDisplayPatternWindow());
 	pattern_handler.AddWindow(window_2);
 
 	wxBoxSizer* sz_1 = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer * sz_1_1 = new wxBoxSizer(wxHORIZONTAL);
 	sz_1_1->Add(window_1, 1, wxEXPAND);
+
 	sz_1_1->Add(window_2, 1, wxEXPAND);
 	
 	sz_1->Add(sz_1_1, 1, wxEXPAND);
